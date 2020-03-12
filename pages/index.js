a89e277068a5b49d07bfda90bfe38b01eb0c04d6
Brian Mathews <brian.mathews@formidable.com>
2020-03-12 13:17:56 -0700

    Simplify example

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
 import Head from 'next/head';
 
 const dynamicScript = `
-	var s = document.createElement('script');
-	s.innerHTML = "console.log('_index log')";
-	document.head.appendChild(s);
+  var s = document.createElement('script');
+  s.innerHTML = "console.log('_index log')";
+  document.head.appendChild(s);
 `;
 
 const Index = () => {
-	return (
-		<>
-			<Head>
-				<title>Index title</title>
-				<script dangerouslySetInnerHTML={{ __html: dynamicScript }} />
-				<meta name="theme-color" content="#ff6868"/>
-			</Head>
-			<div>Hello world</div>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Index title</title>
+        <script dangerouslySetInnerHTML={{ __html: dynamicScript }} />
+        <meta name="theme-color" content="#ff6868"/>
+      </Head>
+      <div>Hello world</div>
+    </>
+  );
 }
 
 export default Index;
